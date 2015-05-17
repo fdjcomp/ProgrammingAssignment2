1ac03b97a25c84743df110ed17b4db713b1066be
fdjcomp <fdjcomp@yahoo.com>
2015-05-17 15:39:28 +0100

    final edit of descriptions of both functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,25 @@
-#Description:
-#Usage:
-#Arguments:
-#Value:
-#References:
-#See Also:
-#Examples:
-
-
 ## Put comments here that give an overall description of what your functions do
 
 #Description: 
-
-#	makeCacheMatrix stores matrix data and specialized matrix functions (to get/set the matrix and its inverse) in a list
-
-#	cacheSolve will takes a list made with makeCacheMatrix as argument, and will return the inverse of the matrix caching
-#	the inverse matrix result, so that it will not have to be recalculated when needed again; 
-#	when the inverse is requested a message ("getting cached matrix inverse") is printed when using the cached result
-
+# makeCacheMatrix stores matrix data and specialized matrix functions (to get/set the matrix and its inverse) in a list
+# cacheSolve a list made with makeCacheMatrix as argument, and will return the inverse of the matrix, caching
+# the inverse matrix result, so that it will not have to be recalculated when needed again; 
+# when the inverse is requested a message ("getting cached matrix inverse") is printed when using the cached result
 
 #Usage:
 # sm<-makeCacheMatrix(matrixM)
 # cacheSolve(sm)
 
 #Example
+
 # > sm<-makeCacheMatrix(matrix(1:4,2,2))
-#first call no message
+#first call to cacheSolve does not print a message
 # > cacheSolve(sm)
 #      [,1] [,2]
 # [1,]   -2  1.5
 # [2,]    1 -0.5
-#second and later calls: the message is printed
+
+#second and later calls: the message is printed ("getting cached inverse")
 # > cacheSolve(sm)
 # getting cached matrix inverse
 #      [,1] [,2]
@@ -42,12 +32,12 @@
 ## Write a short comment describing this function
 
 #Description: store in a list a matrix and special functions to get/set the matrix and its inverse 
-#Argument: a matrix (should be invertible, but is not checked)
-#Value: a list with four items, each a function 
-#	fsetmat: to set the matrix data
-#	fgetmat: to get the matrix data
-#	fgetminv: to get the inverse of the matrix (will be null if inverse not yet calculated)
-#	fsetminv: sets the inverse matrix
+#Argument:    a matrix (should be invertible, but is not checked)
+#Value:       a list with four items, each a function 
+#             fsetmat: to set the matrix data
+#             fgetmat: to get the matrix data
+#             fgetminv: to get the inverse of the matrix (will be null if inverse not yet calculated)
+#             fsetminv: sets the inverse matrix
 
 makeCacheMatrix <- function(mat = matrix()) {
 
@@ -81,9 +71,9 @@ makeCacheMatrix <- function(mat = matrix()) {
 ## Write a short comment describing this function
 
 #Description: return a matrix that is the inverse of the argument
-#Usage: cacheSolve(arg<-makeCacheMatrix(matrixM))
-#Arguments: argument is a list as produced by makeCacheMatrix()
-#Value: returns inverse of matrix stored in argument
+#Usage:       cacheSolve(arg<-makeCacheMatrix(matrixM))
+#Arguments:   argument is a list as produced by makeCacheMatrix()
+#Value:       returns inverse of matrix stored in the argument
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -104,22 +94,5 @@ cacheSolve <- function(x, ...) {
 	
 
 }
-
-
-#could simplify these three lines
-#data <- x$fgetmat()
-#minv <- solve(data)
-#x$fsetminv(minv)
-#to
-#x$fsetminv(solve(x$fgetmat))
-
-
-
-
-
-
-
-
-
 
 
